d4198054552ad30207bee22cdce13bd20ae9c7bb
Davide Rosato <73779437+DavideRosato99@users.noreply.github.com>
2022-01-06 17:48:02 +0100

    update

--- main.m
+++ main.m
@@ -31,7 +31,7 @@ e  = 0.01;                            % [-] Orbit eccentricity
 i  = deg2rad(0);                 % [rad] Orbit innclination
 OM = deg2rad(0);                  % [rad] Orbit RAAN
 om = deg2rad(0);                  % [rad] Orbit pericenter anomaly
-th = 0;                                 % [rad] Orbit true anomaly
+th = deg2rad(0);                                 % [rad] Orbit true anomaly
 
 %%% INERTIAS
 settings.Value.Ix = 1.009;               % [kg m^2] Inertia moment along X axis
@@ -52,7 +52,7 @@ settings.Value.q0 = dcm2quat([1 0 0; 0 1 0; 0 0 1]);
 %%% SENSORS
 settings.Value.SunSensorAccuracy = 0.3;
 settings.Value.SunSensorSampleRate = 50;
-settings.Value.EarthSensorAccuracy = 1;
+settings.Value.EarthSensorAccuracy = 0.25;
 settings.Value.EarthSensorSampleRate = 100;
 settings.Value.GyroscopeARW = 0.2;
 settings.Value.GyroscopeRRW = 0.3;
@@ -66,8 +66,8 @@ settings.Value.ObserverLw = 0.05;
 settings.Value.ObserverLd = 5.8e-4;
 settings.Value.ObserverMd0 = [0 0 0]';
 
-settings.Value.detumblingOnlyMagnMaxTime = 1000;
-settings.Value.detumblingDetumblingMaxTime = 2000;
+settings.Value.detumblingOnlyMagnMaxTime = 1500;
+settings.Value.detumblingDetumblingMaxTime = 2500;
 settings.Value.slewManeuverMaxTime = 3500;
 settings.Value.detumblingkb = 1e7;
 settings.Value.detumblingkp = 0.05;
@@ -121,7 +121,7 @@ settings.Value.omega0 = [settings.Value.wx0 settings.Value.wy0 settings.Value.wz
 % Set the simulation time
 Tperiod = 2*pi * sqrt(a^3/settings.Value.muE);
 % settings.Value.Tsim = settings.Value.Nperiod * Tperiod/5;
-settings.Value.Tsim = 5000;
+settings.Value.Tsim = Tperiod;
 
 %%
 % Simulation run
@@ -129,59 +129,59 @@ simOut = sim('model.slx', 'SrcWorkspace', 'current');
 
 %%
 % Retrieve output data
-T = simOut.tout;            % [s] Simulation time
-R = simOut.Y.Data;
-DCM_I2L = simOut.DCM_I2L.Data;
-DCM_I2B = simOut.DCM_I2B.Data;
-DCM_L2B = simOut.DCM_L2B.Data;
+% T = simOut.tout;            % [s] Simulation time
+% R = simOut.Y.Data;
+% DCM_I2L = simOut.DCM_I2L.Data;
+% DCM_I2B = simOut.DCM_I2B.Data;
+% DCM_L2B = simOut.DCM_L2B.Data;
 
 %% PLOT
-figure;
-quiver3(0,0,0,10000,0,0); grid on; hold on
-axis equal
-quiver3(0,0,0,0,10000,0);
-quiver3(0,0,0,0,0,10000);
-
-% Local
-xl_vec = 5000 .* DCM_I2L(:,:,1)'*[1 0 0]';
-xl = quiver3(R(1,1),R(1,2),R(1,3),xl_vec(1),xl_vec(2),xl_vec(3), 'k');
-yl_vec = 5000 .* DCM_I2L(:,:,1)'*[0 1 0]';
-yl = quiver3(R(1,1),R(1,2),R(1,3),yl_vec(1),yl_vec(2),yl_vec(3), 'k');
-zl_vec = 5000 .* DCM_I2L(:,:,1)'*[0 0 1]';
-zl = quiver3(R(1,1),R(1,2),R(1,3),zl_vec(1),zl_vec(2),zl_vec(3), 'k');
-
-% Body
-xb_vec = 5000 .* DCM_I2B(:,:,1)'*[1 0 0]';
-xb = quiver3(R(1,1),R(1,2),R(1,3),xb_vec(1),xb_vec(2),xb_vec(3), 'b');
-yb_vec = 5000 .* DCM_I2B(:,:,1)'*[0 1 0]';
-yb = quiver3(R(1,1),R(1,2),R(1,3),yb_vec(1),yb_vec(2),yb_vec(3), 'r');
-zb_vec = 5000 .* DCM_I2B(:,:,1)'*[0 0 1]';
-zb = quiver3(R(1,1),R(1,2),R(1,3),zb_vec(1),zb_vec(2),zb_vec(3), 'y');
-
-plot3(R(:,1), R(:,2), R(:,3), '--r', 'LineWidth', 1)
-
-for i = 2:300:length(T)
-    % Local
-    delete(xl)
-    delete(yl)
-    delete(zl)
-    xl_vec = 5000 .* DCM_I2L(:,:,i)'*[1 0 0]';
-    xl = quiver3(R(i,1),R(i,2),R(i,3),xl_vec(1),xl_vec(2),xl_vec(3),'k');
-    yl_vec = 5000 .* DCM_I2L(:,:,i)'*[0 1 0]';
-    yl = quiver3(R(i,1),R(i,2),R(i,3),yl_vec(1),yl_vec(2),yl_vec(3),'k');
-    zl_vec = 5000 .* DCM_I2L(:,:,i)'*[0 0 1]';
-    zl = quiver3(R(i,1),R(i,2),R(i,3),zl_vec(1),zl_vec(2),zl_vec(3),'k');
-
-    % Body
-    delete(xb)
-    delete(yb)
-    delete(zb)
-    xb_vec = 5000 .* DCM_I2B(:,:,i)'*[1 0 0]';
-    xb = quiver3(R(i,1),R(i,2),R(i,3),xb_vec(1),xb_vec(2),xb_vec(3),'b');
-    yb_vec = 5000 .* DCM_I2B(:,:,i)'*[0 1 0]';
-    yb = quiver3(R(i,1),R(i,2),R(i,3),yb_vec(1),yb_vec(2),yb_vec(3),'r');
-    zb_vec = 5000 .* DCM_I2B(:,:,i)'*[0 0 1]';
-    zb = quiver3(R(i,1),R(i,2),R(i,3),zb_vec(1),zb_vec(2),zb_vec(3),'y');
-    drawnow limitrate
-end
+% figure;
+% quiver3(0,0,0,10000,0,0); grid on; hold on
+% axis equal
+% quiver3(0,0,0,0,10000,0);
+% quiver3(0,0,0,0,0,10000);
+% 
+% % Local
+% xl_vec = 5000 .* DCM_I2L(:,:,1)'*[1 0 0]';
+% xl = quiver3(R(1,1),R(1,2),R(1,3),xl_vec(1),xl_vec(2),xl_vec(3), 'k');
+% yl_vec = 5000 .* DCM_I2L(:,:,1)'*[0 1 0]';
+% yl = quiver3(R(1,1),R(1,2),R(1,3),yl_vec(1),yl_vec(2),yl_vec(3), 'k');
+% zl_vec = 5000 .* DCM_I2L(:,:,1)'*[0 0 1]';
+% zl = quiver3(R(1,1),R(1,2),R(1,3),zl_vec(1),zl_vec(2),zl_vec(3), 'k');
+% 
+% % Body
+% xb_vec = 5000 .* DCM_I2B(:,:,1)'*[1 0 0]';
+% xb = quiver3(R(1,1),R(1,2),R(1,3),xb_vec(1),xb_vec(2),xb_vec(3), 'b');
+% yb_vec = 5000 .* DCM_I2B(:,:,1)'*[0 1 0]';
+% yb = quiver3(R(1,1),R(1,2),R(1,3),yb_vec(1),yb_vec(2),yb_vec(3), 'r');
+% zb_vec = 5000 .* DCM_I2B(:,:,1)'*[0 0 1]';
+% zb = quiver3(R(1,1),R(1,2),R(1,3),zb_vec(1),zb_vec(2),zb_vec(3), 'y');
+% 
+% plot3(R(:,1), R(:,2), R(:,3), '--r', 'LineWidth', 1)
+% 
+% for i = 2:300:length(T)
+%     % Local
+%     delete(xl)
+%     delete(yl)
+%     delete(zl)
+%     xl_vec = 5000 .* DCM_I2L(:,:,i)'*[1 0 0]';
+%     xl = quiver3(R(i,1),R(i,2),R(i,3),xl_vec(1),xl_vec(2),xl_vec(3),'k');
+%     yl_vec = 5000 .* DCM_I2L(:,:,i)'*[0 1 0]';
+%     yl = quiver3(R(i,1),R(i,2),R(i,3),yl_vec(1),yl_vec(2),yl_vec(3),'k');
+%     zl_vec = 5000 .* DCM_I2L(:,:,i)'*[0 0 1]';
+%     zl = quiver3(R(i,1),R(i,2),R(i,3),zl_vec(1),zl_vec(2),zl_vec(3),'k');
+% 
+%     % Body
+%     delete(xb)
+%     delete(yb)
+%     delete(zb)
+%     xb_vec = 5000 .* DCM_I2B(:,:,i)'*[1 0 0]';
+%     xb = quiver3(R(i,1),R(i,2),R(i,3),xb_vec(1),xb_vec(2),xb_vec(3),'b');
+%     yb_vec = 5000 .* DCM_I2B(:,:,i)'*[0 1 0]';
+%     yb = quiver3(R(i,1),R(i,2),R(i,3),yb_vec(1),yb_vec(2),yb_vec(3),'r');
+%     zb_vec = 5000 .* DCM_I2B(:,:,i)'*[0 0 1]';
+%     zb = quiver3(R(i,1),R(i,2),R(i,3),zb_vec(1),zb_vec(2),zb_vec(3),'y');
+%     drawnow limitrate
+% end
 
